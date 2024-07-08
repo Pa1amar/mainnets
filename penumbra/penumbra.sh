58a99f21becb0656fb765b2a37e65563dcf33796
Palamar <palamarsat@gmail.com>
2024-07-08 09:32:16 +0300

    Update penumbra.sh

--- penumbra/penumbra.sh
+++ penumbra/penumbra.sh
@@ -43,7 +43,7 @@ mv ~/penumbra/target/release/pd /usr/local/bin/ || exit
 mv ~/penumbra/target/release/pcli /usr/local/bin/ || exit
 pd network unsafe-reset-all
 pcli view reset
-pd network join --external-address $IP_ADDRESS --moniker $MONIKER
+pd network join --external-address $IP_ADDRESS --moniker $MONIKER https://rpc.test-penumbra.theamsolutions.info
 echo -e '\n\e[42mCreating a service for Cometbft Node ...\e[0m\n' && sleep 1
 echo "[Unit]
 Description=Cometbft Node
